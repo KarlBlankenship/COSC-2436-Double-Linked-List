42a62cc3eefb47b99ef86d3bf13fe78032f3b0df
Karl Blankenship <Karleb777@gmail.com>
2018-10-18 16:31:11 -0500

    Created the Node class.

--- src/Double/Linked/List/Insert/DoubleLinkedList.java
+++ src/Double/Linked/List/Insert/DoubleLinkedList.java
@@ -1,16 +1,37 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This program will demonstrate a Double Linked List and an insert method.
  */
 package Double.Linked.List.Insert;
 
 /**
- *
+ * The DoubleLinkedList calls will contain an inner class for double
+ * linked nodes as well as necessary methods for demonstrating the 
+ * insert method. 
  * @author Karl
  */
-public class DoubleLinkedList {
-
+public class DoubleLinkedList 
+{
+    /**
+     * Inner class Node will define the blueprint for double linked list
+     * nodes.
+     */
+    private class Node
+    {
+        String value;   // Value to be stored within a node.
+        Node next;      // Reference variable points to the next node.
+        Node prev;      // Reference variable points to the previous node.
+        
+        /**
+         * Constructor that accepts a String value.
+         * @param val 
+         */
+        Node(String val)
+        {
+            this.value = val;
+        }
+    }
+    
+    
     /**
      * @param args the command line arguments
      */
